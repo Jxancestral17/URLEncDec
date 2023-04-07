c4d88100e3288ced4f4601efe85441d92f0626e4
Federico Manfredi Longo <federicomanfredi.longo@gmail.com>
2023-04-07 12:54:22 +0200

    funzioni base

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/url"
+)
+
+func EncodeURL(text string) string {
+	encText := url.QueryEscape(text)
+	return encText
+}
+
+func DecodeURL(text string) string {
+	decText, err := url.QueryUnescape(text)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return decText
+}
 
 func main() {
-	fmt.Println("test")
+	s := "<scritp>alert(0); document.querySelector('var')</scritp>"
+	encod := EncodeURL(s)
+	decod := DecodeURL(encod)
+	fmt.Println(encod)
+	fmt.Println(decod)
+
 }
